9c3021752132d67b2a919cb4e019113803b22ed1
ahmedmohamed26 <ahmedmohamed26>
2023-08-27 00:03:49 +0300

    add skills anf projects icons

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,9 @@
-import { HomeIcon } from "@/public/svgs";
-import { AboutIcon } from "@/public/svgs";
+import { HomeIcon } from "../../public/svgs";
+import { AboutIcon } from "../../public/svgs";
+import { SkillsIcon } from "../../public/svgs";
+import { ProjectsIcon } from "../../public/svgs";
+
+import Link from "next/link";
 import React from "react";
 
 function Header() {
@@ -7,10 +11,24 @@ function Header() {
     <div className="container h-[70px] bg-white shadow-sm flex items-center mt-5 rounded-md">
       <ul className="flex gap-8 p-5">
         <li>
-          <HomeIcon className="text-stone-500 text-[25px]" />
+          <Link href="/">
+            <HomeIcon className="text-stone-500 text-[25px]" />
+          </Link>
         </li>
         <li>
-          <AboutIcon className="text-stone-500 text-[25px]" />
+          <Link href="/about">
+            <AboutIcon className="text-stone-500 text-[25px]" />
+          </Link>
+        </li>
+        <li>
+          <Link href="/skills">
+            <SkillsIcon className="text-stone-500 text-[25px]" />
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects">
+            <ProjectsIcon className="text-stone-500 text-[25px]" />
+          </Link>
         </li>
       </ul>
     </div>
--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -1,2 +1,4 @@
 export { default as HomeIcon } from "./home.svg";
 export { default as AboutIcon } from "./about.svg";
+export { default as SkillsIcon } from "./skills.svg";
+export { default as ProjectsIcon } from "./projects.svg";
