e956c180a070d4a425c02eec30d60e241a03229c
ahmedmohamed <ahmed.mohamed@afaqy.com>
2023-08-28 18:30:58 +0300

    add info sec

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { MarkerIcon, EmailIcon } from "../public/svgs";
 export default function Home() {
   return (
     <section className="wrapper">
@@ -14,9 +14,7 @@ export default function Home() {
       <div className="profile-details mt-9">
         <div className="info flex justify-between items-top">
           <div>
-            <h1 className="text-4xl  text-slate-800 font-bold font-PoppinsMedium">
-              I'm Ahmed Mohamed
-            </h1>
+            <h1 className="text-4xl  main-color ">I'm Ahmed Mohamed</h1>
             <p className="mt-4 font-PoppinsRegular light-gray-color">
               Frontend Developer From Egypt
             </p>
@@ -39,7 +37,26 @@ export default function Home() {
             className="rounded-lg"
           />
         </div>
-        <ul></ul>
+        <div className="bg-gray-200	p-4 mt-8 rounded-md">
+          <h3 className="main-color">Contact Me</h3>
+          <ul className="mt-5 gap-3 flex flex-col	">
+            <li className="flex items-center">
+              <MarkerIcon className="text-[#4f535f] bg-white p-2 rounded-full w-8 h-8 drop-shadow-md border-zinc-600 " />
+              <span className="mx-3 font-PoppinsRegular light-gray-color ">
+                Cairo , Egypt
+              </span>
+            </li>
+            <li className="flex items-center">
+              <EmailIcon className="text-[#4f535f] bg-white p-2 rounded-full w-8 h-8 drop-shadow-md border-zinc-600 " />
+              <a href={`tel:${+201124269532}`}>
+                {" "}
+                <span className="mx-3 font-PoppinsRegular light-gray-color ">
+                  +20 112 4269 532
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -2,3 +2,5 @@ export { default as HomeIcon } from "./home.svg";
 export { default as AboutIcon } from "./about.svg";
 export { default as SkillsIcon } from "./skills.svg";
 export { default as ProjectsIcon } from "./projects.svg";
+export { default as MarkerIcon } from "./marker.svg";
+export { default as EmailIcon } from "./email.svg";
