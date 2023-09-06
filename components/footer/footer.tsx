142c334586bcc57f6a4155d6668a5b8b5aec35d7
ahmedmohamed <ahmed.mohamed@afaqy.com>
2023-09-06 15:23:45 +0300

    add social media

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,3 +1,10 @@
+import {
+  InstgramIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  GithubIcon,
+} from "@/public/svgs";
+import Link from "next/link";
 import React from "react";
 
 function Footer() {
@@ -7,8 +14,34 @@ function Footer() {
         <h2 className="text-lg main-color font-PoppinsRegular dark:text-light-gray-text">
           Follow Me
         </h2>
-        <ul>
-          <li></li>
+        <ul className="flex gap-3">
+          <li className="text-[#4f535f] bg-white p-2 rounded-full  drop-shadow-md border-zinc-600 dark:bg-soft-gray dark:border-gray-600 dark:border-[1px] dark:text-white">
+            <Link
+              href="https://www.linkedin.com/in/iahmedmohamed/"
+              target="_blank"
+            >
+              <LinkedinIcon />
+            </Link>
+          </li>
+          <li className="text-[#4f535f] bg-white p-2 rounded-full  drop-shadow-md border-zinc-600 dark:bg-soft-gray dark:border-gray-600 dark:border-[1px] dark:text-white">
+            <Link href="https://github.com/ahmedmohamed26" target="_blank">
+              <GithubIcon />
+            </Link>
+          </li>
+          <li className="text-[#4f535f] bg-white p-2 rounded-full  drop-shadow-md border-zinc-600 dark:bg-soft-gray dark:border-gray-600 dark:border-[1px] dark:text-white">
+            <Link href="https://twitter.com/Ahmedmoohamed26" target="_blank">
+              <TwitterIcon />
+            </Link>
+          </li>
+
+          <li className="text-[#4f535f] bg-white p-2 rounded-full  drop-shadow-md border-zinc-600 dark:bg-soft-gray dark:border-gray-600 dark:border-[1px] dark:text-white">
+            <Link
+              href="https://www.instagram.com/ahmed.m.moneem/"
+              target="_blank"
+            >
+              <InstgramIcon />
+            </Link>
+          </li>
         </ul>
       </div>
     </section>
