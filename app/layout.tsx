6188a7ff1cfede0ebf23794eac0fd8ab15de4d5e
ahmedmohamed26 <ahmedmohamed26>
2023-09-07 17:14:42 +0300

    add hotjar script

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,19 @@ export default function RootLayout({
           <Footer />
         </Providers>
       </body>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=3644927"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+   gtag('config', '3644927');`,
+        }}
+      ></script>
     </html>
   );
 }
